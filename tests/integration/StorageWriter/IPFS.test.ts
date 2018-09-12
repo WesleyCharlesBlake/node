b745eac65e5e7b61ee625dda81bd71615ae952d1
Kenneth Lavender <5175120+kennylavender@users.noreply.github.com>
2018-09-12 11:25:34 -0700

    create partial claim tests

---
+++ tests/integration/StorageWriter/IPFS.test.ts
@@ -0,0 +1,67 @@
+import { describe } from 'riteway'
+import { is } from 'ramda'
+import fetch from 'node-fetch'
+
+import { IPFS } from '../../../src/StorageWriter/IPFS'
+import { offendingClaim, workingClaim } from './claims'
+
+const isString = is(String)
+
+const IPFS_URL = 'http://localhost:5001'
+
+const createIPFS = ({
+  ipfsUrl = IPFS_URL
+} = {}) => {
+  return new IPFS({
+    ipfsUrl
+  })
+}
+
+const fetchFile = async (hash: string): Promise<string> => {
+  const response = await fetch(`${IPFS_URL}/api/v0/cat?arg=${hash}`)
+  return response.text()
+}
+
+describe('IPFS.addText', async (should: any) => {
+  const { assert } = should('')
+
+  {
+    const ipfs = createIPFS()
+    const claim = workingClaim
+    const hash = await ipfs.addText(JSON.stringify(claim))
+    assert({
+      given: 'a working claim',
+      should: 'return a hash',
+      actual: isString(hash),
+      expected: true
+    })
+
+    const file = await fetchFile(hash)
+    assert({
+      given: 'the working claims hash',
+      should: 'read the same claim from ipfs',
+      actual: JSON.parse(file),
+      expected: claim
+    })
+  }
+
+  {
+    const ipfs = createIPFS()
+    const claim = offendingClaim
+    const hash = await ipfs.addText(JSON.stringify(claim))
+    assert({
+      given: 'a offending claim',
+      should: 'return a hash',
+      actual: isString(hash),
+      expected: true
+    })
+
+    const file = await fetchFile(hash)
+    assert({
+      given: 'the offending claims hash',
+      should: 'read the same claim from ipfs',
+      actual: JSON.parse(file),
+      expected: claim
+    })
+  }
+})
