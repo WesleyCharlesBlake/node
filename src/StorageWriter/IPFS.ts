db1858f81e78b8e2cc27f136bb626b1812c0c6b6
Kenneth Lavender <5175120+kennylavender@users.noreply.github.com>
2018-09-13 19:52:08 -0700

    fix upload issue

--- src/StorageWriter/IPFS.ts
+++ src/StorageWriter/IPFS.ts
@@ -1,13 +1,12 @@
-import * as FormData from 'form-data'
 import { inject, injectable } from 'inversify'
-import fetch from 'node-fetch'
-import * as str from 'string-to-stream'
+import * as request from 'request'
 
 import { IPFSConfiguration } from './IPFSConfiguration'
 
 /**
  * Wrapper around IPFS' RPC
  */
+
 @injectable()
 export class IPFS {
   private readonly url: string
@@ -16,23 +15,14 @@ export class IPFS {
     this.url = configuration.ipfsUrl
   }
 
-  addText = async (text: string): Promise<string> => {
-    const formData = new FormData() // { maxDataSize: 20971520 }
-
-    formData.append('file', str(text), {
-      knownLength: text.length,
-      filename: 'file',
-      contentType: 'plain/text',
-    })
-
-    const response = await fetch(`${this.url}/api/v0/add`, {
-      method: 'post',
-      body: formData,
-      timeout: 30000,
-    })
-
-    const json = await response.json()
-
-    return json.Hash
-  }
+  addText = (text: string): Promise<string> =>
+    new Promise((resolve, reject) =>
+      request.post(
+        {
+          url: `${this.url}/api/v0/add`,
+          formData: { file: Buffer.from(text) },
+        },
+        (err: any, httpResponse: any, body: any) => (err ? reject('upload failed') : resolve(JSON.parse(body).Hash))
+      )
+    )
 }
